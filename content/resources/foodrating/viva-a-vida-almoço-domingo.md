d85b4633d609374a024dab4224216c4b21b2aaa8
bolokoz <yurifbeck@gmail.com>
2023-02-05 16:20:34 -0400

    tata

---
+++ content/resources/foodrating/viva-a-vida-almoço-domingo.md
@@ -0,0 +1,13 @@
+---
+prato: Almoço Domingo
+restaurante:
+  name: Viva a Vida
+  location: '{"type":"Point","coordinates":[-54.6096953,-20.4664967]}'
+category: self-service
+valor: 55
+nota_sentimental: 1
+nota_tecnica: 2
+date: 2023-02-05T20:17:04.314Z
+delivery: false
+periodo: almoço
+---
---
+++ pages/resources/foodrating/[...slug].vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import type { BlogPost } from '@/types/blog'
+const { path } = useRoute()
+const articles = await queryContent(path).findOne()
+
+const data = computed<BlogPost>(() => {
+  return {
+    title: articles.prato || 'no-title available',
+    description: articles.restaurante.name || 'no-descriptoin available',
+    image: articles.image || '/nuxt-blog/no-image_cyyits.png',
+    alt: articles.title || 'sem foto',
+    ogImage: articles.image || '/nuxt-blog/no-image_cyyits.png',
+    provider: articles.provider || 'cloudinary',
+    date: articles.date || 'not-date-available',
+    tags: articles.tags || [],
+    published: articles.published || false,
+  }
+})
+
+useHead({
+  title: data.value.title || '',
+  meta: [
+    { name: 'description', content: data.value.description },
+    {
+      name: 'description',
+      content: data.value.description,
+    },
+    // Test on: https://developers.facebook.com/tools/debug/ or https://socialsharepreview.com/
+    { property: 'og:site_name', content: `Boloko's Blog` },
+    { hid: 'og:type', property: 'og:type', content: 'website' },
+    {
+      property: 'og:url',
+      content: `https://bolokoz.netlify.app/${path}`,
+    },
+    {
+      property: 'og:title',
+      content: data.value.title,
+    },
+    {
+      property: 'og:description',
+      content: data.value.description,
+    },
+    {
+      property: 'og:image',
+      content: data.value.image || data.value.image,
+    },
+    // Test on: https://cards-dev.twitter.com/validator or https://socialsharepreview.com/
+    { name: 'twitter:site', content: '@bolokozord' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    {
+      name: 'twitter:url',
+      content: `https://boloko.netlify.app/${path}`,
+    },
+    {
+      name: 'twitter:title',
+      content: data.value.title,
+    },
+    {
+      name: 'twitter:description',
+      content: data.value.description,
+    },
+    {
+      name: 'twitter:image',
+      content: data.value.ogImage || data.value.image,
+    },
+  ],
+  link: [
+    {
+      rel: 'canonical',
+      href: `https://bolokoz.netlify.app/${path}`,
+    },
+  ],
+})
+</script>
+
+<template>
+  <main class="px-6 pt-10 container max-w-5xl mx-auto">
+    <header>
+      <h1 class="text-xl md:text-3xl lg:text-5xl m-7 font-bold text-center">
+        {{ data.title || '' }} - restaurante {{ data.description }}
+      </h1>
+      <NuxtImg
+        :src="data.image || ''"
+        :alt="data.alt || ''"
+        class="m-auto rounded-2xl shadow-lg h-52 md:h-96 w-4/5 content-center object-cover"
+      />
+      <!-- <p class="text-xs sm:text-sm my-3 max-w-3xl mx-auto text-center text-zinc-600">
+        {{ data.description }}
+      </p> -->
+      <div class="flex w-full justify-center text-xs md:text-base my-8">
+        <div class="md:flex text-black content-center gap-8 text-xs sm:text-sm">
+          <div class="flex items-center font-semibold">
+            <LogoDate />
+            <p>{{ new Date(data.date).toLocaleDateString('pt-BR') || '' }}</p>
+          </div>
+          <div class="flex items-center gap-2 flex-wrap my-5">
+            <LogoTag />
+            <template v-for="tag in data.tags">
+              <span class="bg-gray-200 rounded-md px-2 py-1 font-semibold">{{ tag }}</span>
+            </template>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div
+      class="prose prose-pre:max-w-xs sm:prose-pre:max-w-full prose-sm sm:prose-base md:prose-lg prose-h1:no-underline max-w-5xl mx-auto prose-zinc prose-img:rounded-lg"
+    >
+      <ContentRenderer :value="articles">
+        <template #empty>
+          <p>No content found.</p>
+        </template>
+      </ContentRenderer>
+    </div>
+  </main>
+</template>
